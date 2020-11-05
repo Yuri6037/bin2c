7a3d0a2b4d478574cf1da16e363cb485f551c21a
Yuri Edward <yuri6037@outlook.com>
2020-11-05 01:47:16 +0100

    Finished mini bin2c

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+#![allow(irrefutable_let_patterns)]
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::vec::Vec;
+use std::env;
+use std::process;
+use std::path::Path;
+use std::io;
+
+fn write_header(const_name: &str, output: &str) -> io::Result<()>
+{
+    let mut outfile = File::create(&Path::new(output))?;
+    let s = format!("#include <stdlib.h>\n\nextern const size_t {}_SIZE;\nextern const char {}[];", const_name, const_name);
+
+    outfile.write(s.as_bytes())?;
+    return Ok(());
+}
+
+fn bin_to_c(const_name: &str, input: &str, output: &str) -> io::Result<()>
+{
+    let mut buffer: [u8;512] = [0; 512];
+    let mut infile = File::open(&Path::new(input))?;
+    let mut outfile = File::create(&Path::new(output))?;
+    let mut globalstr = String::from("#include <stdlib.h>\n\nconst char ") + const_name + "[] = {";
+    let mut globallen: usize = 0;
+
+    while let size = infile.read(&mut buffer)? {
+        if size <= 0 {
+            break;
+        }
+        for i in 0..size {
+            let s = hex::encode([buffer[i]]);
+            globalstr += "0x";
+            globalstr += &s;
+            globalstr += ", ";
+        }
+        globallen += size;
+    }
+    globalstr = String::from(&globalstr[0..globalstr.len() - 2]);
+    globalstr += "};";
+    globalstr += &format!("\nconst size_t {}_SIZE = {};", const_name, globallen);
+    globalstr += "\n";
+    outfile.write(globalstr.as_bytes())?;
+    return Ok(());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let const_name = "BINARY_DATA";
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage <input file> <output file>");
+        process::exit(1);
+    }
+    let cfile = String::from(&args[2]) + ".c";
+    let hfile = String::from(&args[2]) + ".h";
+    match bin_to_c(&const_name, &args[1], &cfile) {
+        Err(e) => {
+            println!("An error has occured: {}", e);
+            process::exit(1);
+        },
+        Ok(()) => println!("Wrote C source")
+    }
+    match write_header(&const_name, &hfile) {
+        Err(e) => {
+            println!("An error has occured: {}", e);
+            process::exit(1);
+        },
+        Ok(()) => println!("Wrote C header")
+    }
 }
---
+++ test.h
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+
+extern const size_t BINARY_DATA_SIZE;
+extern const char BINARY_DATA[];
